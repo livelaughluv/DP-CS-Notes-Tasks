e72c3cdd80c1fce8e36fd165aeb7b65d81437f86
livelaughluv <111532241+livelaughluv@users.noreply.github.com>
2022-08-25 21:29:50 -0500

    Create 08-24-22_4th-Industrial-Revolution.py

---
+++ 08-24-22_4th-Industrial-Revolution.py
@@ -0,0 +1,28 @@
+print("""4th INDUSTRIAL REVOLUTION
+
+The 4th Industrial Revolution is blah blah blah
+
+- MENU -
+`````````
+Please choose one of the options below, and press enter.
+
+1) Question and Answer 1
+2) Question and Answer 2
+3) Question and Answer 3
+""")
+
+answer = "empty"
+
+while (answer == "empty"):
+  userchoice = input()
+  answer = "chosen"
+  
+  if(userchoice == "1"):
+    print("PROMPT 1")
+  elif(userchoice == "2"):
+    print("PROMPT 2")
+  elif(userchoice == "3"):
+    print("PROMPT 3")
+  else:
+    print("That's not a prompt, silly!")
+    answer = "empty"
